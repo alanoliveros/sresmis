cd5b294622f893ea08cb60eac33103639e6d27fe
ricogoron <121242796+ricogoron@users.noreply.github.com>
2023-03-27 21:32:07 +0800

    responsive design sa attendance

--- resources/views/backend/teacher/attendance/attendance.blade.php
+++ resources/views/backend/teacher/attendance/attendance.blade.php
@@ -33,117 +33,25 @@
             <!-- ============================================================== -->
             <div class="row">
                 <!-- Column -->
-                <div class="col-md-6 col-lg-2 col-xlg-3">
-                    <div class="card card-hover">
-                        <div class="box bg-cyan text-center">
-                            <h1 class="font-light text-white">
-                                <i class="mdi mdi-view-dashboard"></i>
-                            </h1>
-                            <h6 class="text-white">Dashboard</h6>
+                <div class="col-md-12 col-lg-12 col-xlg-12">
+                    <div class="card shadow card-hover">
+                        <div class="box bg-cyan">
+                            <h4 class="text-white">Pick Dates</h4>
+                           <div class="row">
+                            <div class="col-8  col-sm-6 col-md-8 mb-1">
+                            <input type="date" class="form-control">
+                            </div>
+                            <div class="col-4  col-sm-4 col-md-2 mb-1">
+                            <a href="" class="btn btn-light  fw-bold">Filter</a>
+                            </div>
+                            <div class="col-12  col-sm-2 col-md-2 mb-1 d-flex justify-content-start  justify-content-md-center">
+                           <a href="" class="btn btn-light fw-bold"><i class="mdi mdi-plus fw-bold"></i>Add</a>
+                            </div>
+                           </div>
                         </div>
                     </div>
                 </div>
-                <!-- Column -->
-                <div class="col-md-6 col-lg-4 col-xlg-3">
-                    <div class="card card-hover">
-                        <div class="box bg-success text-center">
-                            <h1 class="font-light text-white">
-                                <i class="mdi mdi-chart-areaspline"></i>
-                            </h1>
-                            <h6 class="text-white">Charts</h6>
-                        </div>
-                    </div>
-                </div>
-                <!-- Column -->
-                <div class="col-md-6 col-lg-2 col-xlg-3">
-                    <div class="card card-hover">
-                        <div class="box bg-warning text-center">
-                            <h1 class="font-light text-white">
-                                <i class="mdi mdi-collage"></i>
-                            </h1>
-                            <h6 class="text-white">Widgets</h6>
-                        </div>
-                    </div>
-                </div>
-                <!-- Column -->
-                <div class="col-md-6 col-lg-2 col-xlg-3">
-                    <div class="card card-hover">
-                        <div class="box bg-danger text-center">
-                            <h1 class="font-light text-white">
-                                <i class="mdi mdi-border-outside"></i>
-                            </h1>
-                            <h6 class="text-white">Tables</h6>
-                        </div>
-                    </div>
-                </div>
-                <!-- Column -->
-                <div class="col-md-6 col-lg-2 col-xlg-3">
-                    <div class="card card-hover">
-                        <div class="box bg-info text-center">
-                            <h1 class="font-light text-white">
-                                <i class="mdi mdi-arrow-all"></i>
-                            </h1>
-                            <h6 class="text-white">Full Width</h6>
-                        </div>
-                    </div>
-                </div>
-                <!-- Column -->
-                <!-- Column -->
-                <div class="col-md-6 col-lg-4 col-xlg-3">
-                    <div class="card card-hover">
-                        <div class="box bg-danger text-center">
-                            <h1 class="font-light text-white">
-                                <i class="mdi mdi-receipt"></i>
-                            </h1>
-                            <h6 class="text-white">Forms</h6>
-                        </div>
-                    </div>
-                </div>
-                <!-- Column -->
-                <div class="col-md-6 col-lg-2 col-xlg-3">
-                    <div class="card card-hover">
-                        <div class="box bg-info text-center">
-                            <h1 class="font-light text-white">
-                                <i class="mdi mdi-relative-scale"></i>
-                            </h1>
-                            <h6 class="text-white">Buttons</h6>
-                        </div>
-                    </div>
-                </div>
-                <!-- Column -->
-                <div class="col-md-6 col-lg-2 col-xlg-3">
-                    <div class="card card-hover">
-                        <div class="box bg-cyan text-center">
-                            <h1 class="font-light text-white">
-                                <i class="mdi mdi-pencil"></i>
-                            </h1>
-                            <h6 class="text-white">Elements</h6>
-                        </div>
-                    </div>
-                </div>
-                <!-- Column -->
-                <div class="col-md-6 col-lg-2 col-xlg-3">
-                    <div class="card card-hover">
-                        <div class="box bg-success text-center">
-                            <h1 class="font-light text-white">
-                                <i class="mdi mdi-calendar-check"></i>
-                            </h1>
-                            <h6 class="text-white">Calnedar</h6>
-                        </div>
-                    </div>
-                </div>
-                <!-- Column -->
-                <div class="col-md-6 col-lg-2 col-xlg-3">
-                    <div class="card card-hover">
-                        <div class="box bg-warning text-center">
-                            <h1 class="font-light text-white">
-                                <i class="mdi mdi-alert"></i>
-                            </h1>
-                            <h6 class="text-white">Errors</h6>
-                        </div>
-                    </div>
-                </div>
-                <!-- Column -->
+               
             </div>
         </div>
 @endsection

44e87f72fa006e2203fedc41f333ef41e7c413cc
ricogoron <121242796+ricogoron@users.noreply.github.com>
2023-03-27 22:57:43 +0800

    sidebar hover

--- resources/views/layouts/sidebar-component/teacher-sidebar.blade.php
+++ resources/views/layouts/sidebar-component/teacher-sidebar.blade.php
@@ -1,4 +1,4 @@
-<li class="sidebar-item">
+<li class="sidebar-item sidebar_hover">
     <a class="sidebar-link waves-effect waves-dark sidebar-link"
        href="{{route('sresmis.teacher.dashboard')}}"
        aria-expanded="false">
@@ -6,7 +6,7 @@
         <span class="hide-menu">Dashboard</span>
     </a>
 </li>
-<li class="sidebar-item">
+<li class="sidebar-item sidebar_hover">
     <a class="sidebar-link waves-effect waves-dark sidebar-link" href="{{route('sresmis.teacher.attendance')}}"
        aria-expanded="false">
         <i class="mdi mdi-content-paste"></i>
@@ -14,13 +14,13 @@
     </a>
 </li>
 
-<li class="sidebar-item">
+<li class="sidebar-item sidebar_hover">
     <a class="sidebar-link waves-effect waves-dark sidebar-link" href="{{route('sresmis.teacher.grades')}}"
        aria-expanded="false">
         <i class="mdi mdi-table-edit"></i>
         <span class="hide-menu">Grades</span>
     </a>
-</li><li class="sidebar-item">
+</li><li class="sidebar-item sidebar_hover">
     <a class="sidebar-link waves-effect waves-dark sidebar-link" href="{{route('sresmis.teacher.students_information')}}"
        aria-expanded="false">
         <i class="mdi mdi-account-multiple-outline"></i>
@@ -28,20 +28,20 @@
     </a>
 </li>
 
-<li class="sidebar-item">
+<li class="sidebar-item sidebar_hover">
     <a class="sidebar-link has-arrow waves-effect waves-dark" href="javascript:void(0)"
        aria-expanded="false">
         <i class="mdi mdi-file-document-box"></i>
         <span class="hide-menu">School Forms</span>
     </a>
     <ul aria-expanded="false" class="collapse first-level">
-        <li class="sidebar-item">
+        <li class="sidebar-item sidebar_hover">
             <a href="{{route('sresmis.teacher.sf9')}}" class="sidebar-link">
                 <i class="mdi mdi-file"></i>
                 <span class="hide-menu"> School Form 9 </span>
             </a>
         </li>
-        <li class="sidebar-item">
+        <li class="sidebar-item sidebar_hover">
             <a href="icon-fontawesome.html" class="sidebar-link">
                 <i class="mdi mdi-file"></i>
                 <span class="hide-menu"> School Form 2 </span>
@@ -51,20 +51,20 @@
 </li>
 
 
-<li class="sidebar-item">
+<li class="sidebar-item sidebar_hover">
     <a class="sidebar-link has-arrow waves-effect waves-dark" href="javascript:void(0)"
        aria-expanded="false">
         <i class="mdi mdi-move-resize-variant"></i>
         <span class="hide-menu">Summaries </span>
     </a>
     <ul aria-expanded="false" class="collapse first-level">
-        <li class="sidebar-item">
+        <li class="sidebar-item sidebar_hover">
             <a href="index2.html" class="sidebar-link">
                 <i class="mdi mdi-view-dashboard"></i>
                 <span class="hide-menu">Quarter Grades</span>
             </a>
         </li>
-        <li class="sidebar-item">
+        <li class="sidebar-item sidebar_hover">
             <a href="pages-gallery.html" class="sidebar-link">
                 <i class="mdi mdi-file-outline"></i>
                 <span class="hide-menu">Attendance Report</span>

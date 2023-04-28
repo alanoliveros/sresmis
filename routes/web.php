b133e9208476508f3316bcd9b755119bdb40a02f
Alan Supla-ag Oliveros <71147809+alanoliveros@users.noreply.github.com>
2023-04-29 07:04:27 +0800

    ahh web web

--- routes/web.php
+++ routes/web.php
@@ -23,8 +23,6 @@ Route::get('/', function () {
 
 Auth::routes();
 
-
-
 /** Admin Controller */
 Route::prefix('sresmis/admin')->middleware('isAdmin')->group(function () {
     Route::get('/', [AdminController::class, 'index'])->name('sresmis.admin.dashboard');
@@ -32,96 +30,61 @@ Route::prefix('sresmis/admin')->middleware('isAdmin')->group(function () {
     Route::post('/add-teacher', [AdminController::class, 'addTeacher'])->name('sresmis.admin.add-teacher');
 });
 
-
-/*Route::get('/sresmis/admin/dashboard', [App\Http\Controllers\AdminController::class, 'index'])->name('sresmis.admin.dashboard')->middleware('isAdmin');
-Route::get('/sresmis/admin/teachers', [App\Http\Controllers\AdminController::class, 'teachers'])->name('sresmis.admin.teachers')->middleware('isAdmin');
-Route::post('/sresmis/admin/add-teacher', [App\Http\Controllers\AdminController::class, 'addTeacher'])->name('sresmis.admin.add-teacher')->middleware('isAdmin');*/
-
-
-
-
 /** Key Performance Indicator */
 Route::middleware('isAdmin')->resource('key-performance-indicator', PerformanceIndicatorController::class);
 
-/*Route::prefix('key-performance-indicator')->middleware('isAdmin')->group(function () {
-    Route::get('/', [PerformanceIndicatorController::class, 'index'])->name('key-performance-indicator.index');
-    Route::get('/create', [PerformanceIndicatorController::class, 'create'])->name('key-performance-indicator.create');
-    Route::post('/store', [PerformanceIndicatorController::class, 'store'])->name('key-performance-indicator.store');
-    Route::get('/{id}/edit', [PerformanceIndicatorController::class, 'edit'])->name('key-performance-indicator.edit');
-    Route::put('/{id}/update', [PerformanceIndicatorController::class, 'update'])->name('key-performance-indicator.update');
-    Route::delete('/{id}/destroy', [PerformanceIndicatorController::class, 'destroy'])->name('key-performance-indicator.destroy');
-});*/
+/** Subjects */
+Route::prefix('sresmis/admin')->middleware('isAdmin')->group(function () {
+    Route::get('/manage-subjects', [AdminController::class, 'manageSubjects'])->name('manage-subjects');
+    Route::get('/{name}/{id}', [AdminController::class, 'addsubjectByGradeLevel']);
+    Route::post('/add-subjectBygradeLevel', [AdminController::class, 'add_subjectBygradeLevel'])->name('add-subjectBygradeLevel');
+});
+
+/** Section */
+Route::prefix('sresmis/admin')->middleware('isAdmin')->group(function () {
+    Route::post('/getSection', [AdminController::class, 'getSection']);
+    Route::get('/manage-sections', [AdminController::class, 'manageSections'])->name('manage-sections');
+    Route::post('/create-section', [AdminController::class, 'create_section'])->name('create-section');
+});
+
+/** Manage Class Schedules */
+Route::prefix('sresmis/admin')->middleware('isAdmin')->group(function () {
+    Route::get('/manage-class-schedules', [AdminController::class, 'manage_class_schedules'])->name('manage-class-schedules');
+    Route::get('/schedules/view-by-gradelevel/{name}', [AdminController::class, 'view_by_gradeLevel']);
+    Route::get('/schedules/view-by-section/{sid}/{gid}', [AdminController::class, 'view_by_section']);
+    Route::post('/schedules/add-schedule-by-section', [AdminController::class, 'add_schedule_by_section'])->name('add-schedule-by-section');
+});
+
+
+Route::prefix('sresmis/teacher')->middleware('isTeacher')->group(function () {
+    /** Teacher Controller */
+    Route::get('/dashboard', [App\Http\Controllers\TeacherController::class, 'index'])->name('sresmis.teacher.dashboard');
+    Route::get('/advisory', [App\Http\Controllers\TeacherController::class, 'advisory'])->name('sresmis.teacher.advisory');
+    Route::get('/grades/filter', [App\Http\Controllers\TeacherController::class, 'filterGrades'])->name('sresmis.teacher.grades.filter');
+
+    /** Add Student */
+    Route::post('/add-student', [App\Http\Controllers\TeacherController::class, 'addStudent'])->name('sresmis.teacher.add-student');
+    Route::get('/delete-student/{id}', [App\Http\Controllers\TeacherController::class, 'deleteStudent']);
+
+    /** Teacher Attendance */
+    Route::get('/attendance/advisory', [App\Http\Controllers\TeacherController::class, 'attendance'])->name('sresmis.teacher.attendance.by-advisory');
+    Route::get('/grades', [App\Http\Controllers\TeacherController::class, 'grades'])->name('sresmis.teacher.grades');
+    Route::get('/students-information', [App\Http\Controllers\TeacherController::class, 'students_information'])->name('sresmis.teacher.students_information');
+
+    /** Teacher School Forms */
+    Route::get('/school-form-1', [App\Http\Controllers\SchoolForm1::class, 'sf1'])->name('sresmis.teacher.sf1');
+    Route::post('/get-student-sf1-by-school-year', [App\Http\Controllers\SchoolForm1::class, 'get_student_sf1_by_sy']);
+    Route::get('/school-form-9', [App\Http\Controllers\TeacherController::class, 'sf9'])->name('sresmis.teacher.sf9');
+
+    /** Manage Class Schedules */
+    Route::get('/class-schedule', [App\Http\Controllers\TeacherController::class, 'class_schedule'])->name('sresmis.teacher.class-schedule');
+});
 
 
 
-
-
-
-
-
-
-
-// subjects
-Route::get('/sresmis/admin/manage-subjects', [App\Http\Controllers\AdminController::class, 'manageSubjects'])->name('manage-subjects')->middleware('isAdmin');
-Route::get('/sresmis/admin/{name}/{id}', [App\Http\Controllers\AdminController::class, 'addsubjectByGradeLevel'])->middleware('isAdmin');
-Route::post('/sresmis/admin/add-subjectBygradeLevel', [App\Http\Controllers\AdminController::class, 'add_subjectBygradeLevel'])->name('add-subjectBygradeLevel')->middleware('isAdmin');
-// section
-Route::post('/sresmis/admin/getSection', [App\Http\Controllers\AdminController::class, 'getSection'])->middleware('isAdmin');
-Route::get('/sresmis/admin/manage-sections', [App\Http\Controllers\AdminController::class, 'manageSections'])->name('manage-sections')->middleware('isAdmin');
-Route::post('/sresmis/admin/create-section', [App\Http\Controllers\AdminController::class, 'create_section'])->name('create-section')->middleware('isAdmin');
-// end section
-
-// manage class schedules
-Route::get('/sresmis/admin/manage-class-schedules', [App\Http\Controllers\AdminController::class, 'manage_class_schedules'])->name('manage-class-schedules')->middleware('isAdmin');
-Route::get('/sresmis/admin/schedules/view-by-gradelevel/{name}', [App\Http\Controllers\AdminController::class, 'view_by_gradeLevel'])->middleware('isAdmin');
-Route::get('/sresmis/admin/schedules/view-by-section/{sid}/{gid}', [App\Http\Controllers\AdminController::class, 'view_by_section'])->middleware('isAdmin');
-Route::post('/sresmis/admin/schedules/add-schedule-by-section', [App\Http\Controllers\AdminController::class, 'add_schedule_by_section'])->name('add-schedule-by-section')->middleware('isAdmin');
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// TeacherController
-Route::get('/sresmis/teacher/dashboard', [App\Http\Controllers\TeacherController::class, 'index'])->name('sresmis.teacher.dashboard')->middleware('isTeacher');
-Route::get('/sresmis/teacher/advisory', [App\Http\Controllers\TeacherController::class, 'advisory'])->name('sresmis.teacher.advisory')->middleware('isTeacher');
-Route::get('/sresmis/teacher/grades/filter', [App\Http\Controllers\TeacherController::class, 'filterGrades'])->name('sresmis.teacher.grades.filter')->middleware('isTeacher');
-// add student
-Route::post('/sresmis/teacher/add-student', [App\Http\Controllers\TeacherController::class, 'addStudent'])->name('sresmis.teacher.add-student')->middleware('isTeacher');
-Route::get('/sresmis/teacher/delete-student/{id}', [App\Http\Controllers\TeacherController::class, 'deleteStudent'])->middleware('isTeacher');
-
-// ParentController
+/** Student Dashboard */
 Route::get('/sresmis/student/dashboard', [App\Http\Controllers\TeacherController::class, 'index'])->name('sresmis.student.dashboard')->middleware('isStudent');
 
-// ParentController
+/** Parent Dashboard */
 Route::get('/sresmis/parent/dashboard', [App\Http\Controllers\TeacherController::class, 'index'])->name('sresmis.parent.dashboard')->middleware('isParent');
-
-
-
-Route::get('/sresmis/teacher/attendance/advisory', [App\Http\Controllers\TeacherController::class, 'attendance'])->name('sresmis.teacher.attendance.by-advisory')->middleware('isTeacher');
-Route::get('/sresmis/teacher/grades', [App\Http\Controllers\TeacherController::class, 'grades'])->name('sresmis.teacher.grades')->middleware('isTeacher');
-Route::get('/sresmis/teacher/students-information', [App\Http\Controllers\TeacherController::class, 'students_information'])->name('sresmis.teacher.students_information')->middleware('isTeacher');
-
- // teacher school forms
-Route::get('/sresmis/teacher/school-form-1', [App\Http\Controllers\SchoolForm1::class, 'sf1'])->name('sresmis.teacher.sf1')->middleware('isTeacher');
-Route::post('/sresmis/teacher/get-student-sf1-by-school-year', [App\Http\Controllers\SchoolForm1::class, 'get_student_sf1_by_sy'])->middleware('isTeacher');
-Route::get('/sresmis/teacher/school-form-9', [App\Http\Controllers\TeacherController::class, 'sf9'])->name('sresmis.teacher.sf9')->middleware('isTeacher');
-
-
-// manage class Schedules
-Route::get('/sresmis/teacher/class-schedule', [App\Http\Controllers\TeacherController::class, 'class_schedule'])->name('sresmis.teacher.class-schedule')->middleware('isTeacher');
 

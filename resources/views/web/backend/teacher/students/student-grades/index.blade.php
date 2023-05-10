c71d769d493e6c5804da7e1d8f59969178ee57ba
ricogoron <121242796+ricogoron@users.noreply.github.com>
2023-05-10 17:28:06 +0800

    alan gwapo

--- resources/views/web/backend/teacher/students/student-grades/index.blade.php
+++ resources/views/web/backend/teacher/students/student-grades/index.blade.php
@@ -55,28 +55,35 @@
                                             class="btn btn-secondary rounded-0 filter_grades">Filter</button>
                                     </div>
                                 </div>
-                                <div class="col-12 border border-dark">
-                                   <table class="table table-hover">
+                                <div class="col-12 border border-dark mb-4">
+                                    <table class="table table-hover">
                                         <thead>
                                             <tr>
-                                                <th colspan="6" class="text-center fs-4">Written Works ( <u class="text-success"><small class="">40</small><small>%</small></u> )</th>
+                                                <th colspan="6" class="text-center fs-4">Written Work ( <u
+                                                        class="text-success"><small
+                                                            class="">40</small><small>%</small></u> )</th>
                                             </tr>
                                             <tr>
                                                 <th></th>
-                                                <th class="text-center">1</th>
-                                                <th class="text-center">Total</th>
+                                                <th class="text-center fw-bold">1</th>
+                                                <th class="text-center between_count">Total</th>
                                                 <th class="text-center">PS</th>
                                                 <th class="text-center">WA</th>
                                                 <th class="text-center">Action</th>
-                                                
+
                                             </tr>
                                             <tr>
                                                 <th>Highest Possible Score</th>
-                                                <th><input type="number" name="score[1]" min="1" class="form-control" ></th>
-                                                <th class="text-center text-success"><span class="highest_total_written total_score"></span></th>
-                                                <th class="text-center text-success"><span class="percentage_score_written">100</span></th>
-                                                <th class="text-center text-success"><span class="weighted_score_written">40%</span></th>
-                                                <th class="text-center"><button type="button" class="btn btn-success">+</button></th>
+                                                <th><input type="number" name="score[]" min="1" 
+                                                        class="form-control written_output_score"></th>
+                                                <th class="text-center text-success between_highest_score"><span
+                                                        class="total_score">0</span></th>
+                                                <th class="text-center text-success"><span
+                                                        class="percentage_score_written">100</span></th>
+                                                <th class="text-center text-success"><span
+                                                        class="weighted_score_written">40%</span></th>
+                                                <th class="text-center"><button type="button"
+                                                        class="btn btn-success add_score">+</button></th>
                                             </tr>
                                         </thead>
                                         <tbody>
@@ -86,17 +93,80 @@
                                                         <option selected disabled>Select Student</option>
                                                     </select>
                                                 </td>
-                                                <td><input type="number" min="1" class="form-control fs-6" placeholder="Enter Score"></td>
+                                                <td><input type="number" min="1" name="student_score[]"
+                                                        class="form-control fs-6 quizzes_exams" placeholder="Enter Score">
+                                                </td>
+                                                <td class="total_score_by_learner text-center"><span
+                                                        class="put_total_score_by_student"></span></td>
+                                                <td class="text-center"><span class="percentage_score_by_student"></span>
+                                                </td>
+                                                <td class="text-center"><span
+                                                        class="weighted_score_written_by_student"></span></td>
+                                                <td></td>
                                             </tr>
                                         </tbody>
-                                   </table>
+                                    </table>
                                 </div>
+
+                                {{-- perforamnce Tasks --}}
+                                <div class="col-12 border border-dark mb-4">
+                                    <table class="table table-hover">
+                                        <thead>
+                                            <tr>
+                                                <th colspan="6" class="text-center fs-4">Performance Task ( <u
+                                                        class="text-success"><small
+                                                            class="">40</small><small>%</small></u> )</th>
+                                            </tr>
+                                            <tr>
+                                                <th></th>
+                                                <th class="text-center fw-bold">1</th>
+                                                <th class="text-center between_count">Total</th>
+                                                <th class="text-center">PS</th>
+                                                <th class="text-center">WA</th>
+                                                <th class="text-center">Action</th>
+
+                                            </tr>
+                                            <tr>
+                                                <th>Highest Possible Score</th>
+                                                <th><input type="number" name="score[]" min="1" 
+                                                        class="form-control written_output_score"></th>
+                                                <th class="text-center text-success between_highest_score"><span
+                                                        class="total_score">0</span></th>
+                                                <th class="text-center text-success"><span
+                                                        class="percentage_score_written">100</span></th>
+                                                <th class="text-center text-success"><span
+                                                        class="weighted_score_written">40%</span></th>
+                                                <th class="text-center"><button type="button"
+                                                        class="btn btn-success add_score">+</button></th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>
+                                                    <select name="" id="" class="form-control">
+                                                        <option selected disabled>Select Student</option>
+                                                    </select>
+                                                </td>
+                                                <td><input type="number" min="1" name="student_score[]"
+                                                        class="form-control fs-6 quizzes_exams" placeholder="Enter Score">
+                                                </td>
+                                                <td class="total_score_by_learner text-center"><span
+                                                        class="put_total_score_by_student">0</span></td>
+                                                <td class="text-center"><span class="percentage_score_by_student"></span>
+                                                </td>
+                                                <td class="text-center"><span
+                                                        class="weighted_score_written_by_student"></span></td>
+                                                <td></td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
                                 </div>
                             </div>
                         </div>
-
                     </div>
-                </div><!-- End Recent Sales -->
+
+                </div>
+            </div><!-- End Recent Sales -->
 
             </div>
         </section>
@@ -108,14 +178,63 @@
         let sub_id = 0;
         let grade_component = 0;
         let first_score = 0;
-        function gradeComponent(val){
-           
-            console.log(val);
+        let score_count = 1;
+        let total_score = [];
+        let sum = 0;
+        let total = 0;
+
+
+        function gradeComponent() {
+            let activity = $('.written_output_score').length;
+            if (activity == 1) {
+                first_score = $('.written_output_score').val();
+                $('.total_score').text(first_score);
+            } else {
+
+            }
         }
-        
+
+        function totalHighestScore() {
+            sum = 0;
+            $.each($(".written_output_score"), function(key, val) {
+                sum += parseInt($(val).val());
+
+            });
+            $('.total_score').text(sum);
+        }
+
+        function student_score() {
+            total = 0;
+            $.each($(".quizzes_exams"), function(key, val) {
+                total += parseInt($(val).val());
+
+            });
+            $('.put_total_score_by_student').text(total);
+            let ps = parseInt(total / sum * 100);
+            $('.percentage_score_by_student').text(ps);
+            let ws = 0.5;
+            $('.weighted_score_written_by_student').text(ps * ws + '%');
+
+
+
+
+        }
+
         $(document).ready(function() {
+            gradeComponent();
+            $('.add_score').on('click', function() {
+
+                $(`<th class="text-center">${++score_count}</th>`).insertBefore('.between_count');
+                $(`<th class="score_append fw-bold" data-rows="score_append_${score_count}"><input type="number"  name="score[]" min="1" class="form-control written_output_score"></th>`)
+                    .insertBefore('.between_highest_score');
+                $(`<td><input type="number"  min="1" name="student_score[]" class="form-control fs-6 quizzes_exams" placeholder="Enter Score"></td>`)
+                    .insertBefore('.total_score_by_learner');
+
+
+            });
+
             grade_component = $('.grade_component').text();
-            gradeComponent(grade_component);
+
             $(".select_sy").on('change', function() {
                 sy_id = $(".select_sy :selected").val();
                 if (sy_id != 0 && sub_id != 0) {
@@ -128,6 +247,7 @@
                     $('.filter_grades').prop('disabled', false);
                 }
             });
+
             $(".filter_grades").on('click', function(e) {
                 e.preventDefault();
                 $.ajax({
@@ -142,14 +262,21 @@
                     }
                 });
             });
-            $("input[name='score[1]']").on('change', function(e){
-                first_score = $(this).val();
-                $('.total_score').text(first_score);
+
+            $("body").on('input', "input[name='score[]']", function(e) {
+
+                totalHighestScore();
+                let val = $(this).val();
+                $(this).attr('data-info', val);
+
             });
+            $("body").on('input', "input[name='student_score[]']", function(e) {
 
-            
+                student_score();
+                let val = $(this).val();
+                $(this).attr('data-info', val);
 
-           
+            });
         });
     </script>
 @endsection

0d42944126925930f4bb0066d9629b199dd043f0
Alan Supla-ag Oliveros <71147809+alanoliveros@users.noreply.github.com>
2023-05-11 01:02:33 +0800

    tabang

--- resources/views/web/backend/admin/users/teacher/index.blade.php
+++ resources/views/web/backend/admin/users/teacher/index.blade.php
@@ -20,7 +20,18 @@
 
                     <div class="card">
                         <div class="card-body">
-                            <h5 class="card-title">@yield('title')</h5>
+
+                            {{--<h5 class="card-title">@yield('title')</h5>--}}
+
+                            <div class="card-title">
+                                <span class="fs-4">@yield('title')</span>
+                                <a href="" class="btn btn-primary float-end" data-bs-toggle="modal"
+                                   data-bs-target="#addTeacher">+ Add teacher</a>
+
+                                {{--@include('backend.admin.teachers.add-teacher')--}}
+                            </div>
+
+
                             {{--<p>Add lightweight datatables to your project with using the <a href="https://github.com/fiduswriter/Simple-DataTables" target="_blank">Simple DataTables</a> library. Just add <code>.datatable</code> class name to any table you wish to conver to a datatable</p>--}}
 
                             <!-- Table with stripped rows -->
@@ -28,20 +39,35 @@
                                 <thead>
                                 <tr>
                                     <th scope="col">#</th>
-                                    <th scope="col">Name</th>
-                                    <th scope="col">Position</th>
-                                    <th scope="col">Age</th>
-                                    <th scope="col">Start Date</th>
+                                    <th scope="col">Full name</th>
+                                    <th scope="col">Designation</th>
+                                    <th scope="col">Grade Level Taught</th>
+                                    <th scope="col">Section Assigned</th>
+                                    <th scope="col">Action</th>
+
                                 </tr>
                                 </thead>
                                 <tbody>
-                                <tr>
-                                    <th scope="row">1</th>
-                                    <td>Brandon Jacob</td>
-                                    <td>Designer</td>
-                                    <td>28</td>
-                                    <td>2016-05-25</td>
-                                </tr>
+                                @foreach($teachers as $key=>$teacher)
+                                    <tr>
+                                        <th scope="row">{{$key+1}}</th>
+                                        <td>{{$teacher->lastname.', '.($teacher->middlename == NULL? '':$teacher->middlename.', ').$teacher->name.($teacher->suffix == NULL? '':', '.$teacher->suffix)}}</td>
+                                        <td>{{$teacher->designation}}</td>
+                                        <td>{{$teacher->gradeLevelName}}</td>
+                                        <td>{{$teacher->sectionName}}</td>
+                                        <td>
+                                            <div class="dropdown" tabindex="1">
+                                                <i class="db2" tabindex="1"></i>
+                                                <a class="dropbtn"><i class=" fs-4 mdi mdi-dots-vertical"></i></a>
+                                                <div class="dropdown-content">
+                                                    <a href="#">View</a>
+                                                    <a href="#">Edit</a>
+                                                    <a href="#" class="text-danger">Delete</a>
+                                                </div>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                @endforeach
 
                                 </tbody>
                             </table>

6e8e73e0427c3a187d88f83a5592b5de90ffbdc9
Alan Supla-ag Oliveros <71147809+alanoliveros@users.noreply.github.com>
2023-05-22 18:42:21 +0800

    way tubigg

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        DB::table('enrollments')->truncate();
+
         /*DB::table('users')->insert([
             [
                 'name' => 'Admin User',
